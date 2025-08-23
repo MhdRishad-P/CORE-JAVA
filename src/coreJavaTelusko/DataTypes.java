05ae3ea9557ec8b426acff2d8e1db5995b38311d
MuhammedRishad-P <muhammedrishad4319@gmail.com>
2025-08-23 14:01:05 +0530

    Basics of Java

---
+++ src/coreJavaTelusko/DataTypes.java
@@ -0,0 +1,24 @@
+package coreJavaTelusko;
+
+public class DataTypes {
+	public static void main(String[] args) {
+//		----primitive DataType
+//		Integer-byte,short,int,long
+//		Float-double,float
+//		Character-char
+//		Boolean-true,false
+		int num1=9;
+		byte by=127;
+		short sh=558;
+		long l=5854l;
+		
+		float f=6.7f;
+		double db =6.77777;
+		
+		char ch='b';
+		
+		boolean bool=true;
+		
+	}
+
+}
---
+++ src/coreJavaTelusko/Variables.java
@@ -0,0 +1,14 @@
+package coreJavaTelusko;
+
+public class Variables {
+	public static void main(String[] args) {
+//	     Syntax- DataType VariableName = value;
+		int num1 =5;
+		int num2=8;
+		int result=num1+num2;
+		System.out.println(result);
+		
+	}
+
+	
+}
